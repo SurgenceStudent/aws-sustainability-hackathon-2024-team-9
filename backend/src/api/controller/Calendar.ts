29157161490d87a4ee4c17d61d55923a95b84244
Ali Hasan <aliimadhasan97@gmail.com>
2024-10-05 13:27:17 -0700

    Fixed type errors

--- backend/src/api/controller/Calendar.ts
+++ backend/src/api/controller/Calendar.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express";
 import { Day } from "../model/Day";
+import { CalendarEvent } from "../model/CalendarEvent";
+import { TM_TO_VERB, TransportMode } from "../model/TransportMode";
+import { TransportationEvent } from "../model/TransportationEvent";
 
 export async function analyzeDay(req: Request, res: Response) {
   const { day } = req.body;
@@ -11,6 +14,24 @@ export async function analyzeDay(req: Request, res: Response) {
 export function printDayView(day: Day) {
   console.log(`Day: ${day.date.toDateString()}`);
   day.events.forEach(event => {
-    console.log(`${event.start.toLocaleTimeString()} - ${event.end.toLocaleTimeString()}: ${event.name} at ${event.address}`);
+    if ("from" in event) {
+      console.log(`${event.start.toLocaleTimeString()} - ${event.end.toLocaleTimeString()}: ${event.name} at ${event.from}`);
+    } else {
+      console.log(`${event.start.toLocaleTimeString()} - ${event.end.toLocaleTimeString()}: ${event.name} at ${event.address}`);
+    }
   });
+}
+
+// transforms a day by including a transportation event from 'from' to 'to'
+export async function addTransportationEvent(day: Day, from: CalendarEvent, to: CalendarEvent, mode: TransportMode): Promise<Day> {
+  const transportationEvent: TransportationEvent = {
+    name: `Taking a ${TM_TO_VERB[mode]} ${from.name} to ${to.name}`,
+    mode: mode,
+    start: from.end,
+    end: to.start,
+    from: from.address,
+    to: to.address,
+  };
+  day.events.push(transportationEvent);
+  return day;
 }
--- backend/src/api/model/Day.ts
+++ backend/src/api/model/Day.ts
@@ -1,8 +1,9 @@
 // Description: Represents a day in the calendar with a series of events
 
 import { CalendarEvent } from "./CalendarEvent";
+import { TransportationEvent } from "./TransportationEvent";
 
 export interface Day {
   date: Date;  // day this represents, time does not matter
-  events: Array<CalendarEvent> // list of events in this day
+  events: Array<CalendarEvent | TransportationEvent> // list of events in this day
 }
